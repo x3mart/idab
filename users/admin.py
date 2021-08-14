9e0c7be07ae0a6f7cfe91c5d0046d257c4f64fda
x3mart <x3mat@gmail.com>
2021-08-14 17:33:54 +0300

    admin user creation form

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,44 @@
 from django.contrib import admin
+from django import forms
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Student, Teacher, Manager
 from courses.models import Course
 
+class UserCreationForm(forms.ModelForm):
+    """A form for creating new users. Includes all the required
+    fields, plus a repeated password."""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('email', 'name', 'avatar', 'is_superuser', 'is_staff', 'is_active')
+
+    def clean_password2(self):
+        # Check that the two password entries match
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save the provided password in hashed format
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
 class MyUserAdmin(UserAdmin):
+    add_form = UserCreationForm
     ordering = ('email',)
     fieldsets = ((None, {'fields':('name', 'email', 'password', 'avatar', 'is_superuser', 'is_staff', 'is_active')}),)
-    add_fieldsets = ((None, {'fields':('name', 'email', 'password', 'avatar', 'is_staff', 'is_active')}),)
+    add_fieldsets = (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'avatar', 'is_superuser', 'is_staff', 'is_active'),
+        }),
     list_display = ('name', 'email', 'is_superuser', 'is_staff', 'is_active',)
 
 
@@ -16,12 +48,16 @@ class CourseInline(admin.TabularInline):
     verbose_name = 'Курс'
     verbose_name_plural = 'Читаемые курсы'
 
-
+class TeacherCreationForm(UserCreationForm):
+    class Meta:
+        model = Teacher
+        fields = ('email', 'name', 'is_active', 'short_position', 'full_position', 'description', 'link', 'avatar')
 
 class TeacherAdmin(UserAdmin):
+    add_form = TeacherCreationForm
     ordering = ('email',)
     fieldsets = ((None, {'fields':('name', 'email', 'password', 'is_active', 'short_position', 'full_position', 'description', 'link', 'avatar')}),)
-    add_fieldsets = ((None, {'fields':('name', 'email', 'password', 'is_active', 'short_position', 'full_position', 'description', 'link', 'avatar')}),)
+    add_fieldsets = ((None, {'fields':('email', 'name', 'password1', 'password2', 'is_active', 'short_position', 'full_position', 'description', 'link', 'avatar')}),)
     list_display = ('name', 'email', 'is_active', 'on_site')
     list_editable = ('is_active', 'on_site')
     inlines = [
@@ -41,7 +77,7 @@ class ManagerAdmin(admin.ModelAdmin):
     get_name.short_description = 'Имя'
     get_is_active.short_description = 'is_active'
 
-admin.site.register(User)
+admin.site.register(User, MyUserAdmin)
 admin.site.register(Student)
 admin.site.register(Manager, ManagerAdmin)
 admin.site.register(Teacher, TeacherAdmin)
