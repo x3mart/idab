caad86dccfba04729c80255ca74f1d308703ec07
viperovm <viperovm@gmail.com>
2021-08-26 19:44:30 +0300

    admin style init

--- idab/urls.py
+++ idab/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -25,6 +26,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('__debug__/', include(debug_toolbar.urls)),
     # path to djoser end points
     path('auth/', include('djoser.urls')),
     # path('auth/', include('djoser.urls.jwt')),
