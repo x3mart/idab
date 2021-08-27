dbb142a8deb12f0c3fd12e9a85c2bfbc764d5bd3
x3mart <x3mat@gmail.com>
2021-08-27 12:20:49 +0300

    debugbar fix

--- idab/urls.py
+++ idab/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
+# import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -43,7 +43,7 @@ urlpatterns = [
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
+    # urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
     # urlpatterns += url_docs
 urlpatterns += url_docs
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
