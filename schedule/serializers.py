cea38062c46ef44bac88f4fda21af23e9db06129
x3mart <x3mat@gmail.com>
2021-09-22 23:21:30 +0300

    schedule

--- schedule/serializers.py
+++ schedule/serializers.py
@@ -3,11 +3,13 @@ from rest_framework import serializers
 from .models import Schedule
 from users.models import Teacher
 from checkpoints.models import Checkpoint
+from courses.models import Course
 
 
 class LkScheduleSerializer(serializers.ModelSerializer):
     teacher = serializers.CharField(read_only=True, source='teacher.name')
-    training_group = serializers.CharField(read_only=True, source='training_group.__str__')
+    training_group = serializers.CharField(read_only=True)
+    course = serializers.CharField(read_only=True)
     class Meta:
         model = Schedule
         fields = '__all__'
@@ -17,48 +19,50 @@ class LkScheduleSerializer(serializers.ModelSerializer):
         training_group = request.data.get('training_group')
         teacher = request.data.get('teacher')
         checkpoint = request.data.get('checkpoint')
+        course = request.data.get('course')
         if training_group is not None:
-            training_groups = Teacher.objects.get(pk=training_group)
+            training_group = TrainingGroup.objects.get(pk=training_group)
         else:
             raise serializers.ValidationError({
                 'training_group': 'Обязательное поле!'
             })
         if teacher is not None:
-            teacher = TrainingGroup.objects.get(pk=teacher)
+            teacher = Teacher.objects.get(pk=teacher)
         else:
             raise serializers.ValidationError({
                 'teacher': 'Обязательное поле!'
             })
-        shedule = Schedule(**validated_data)
-        shedule.save()
+        if course is not None:
+            course = Course.objects.get(pk=course)
+        else:
+            raise serializers.ValidationError({
+                'course': 'Обязательное поле!'
+            })
+        schedule = Schedule(**validated_data)
+        schedule.training_group=training_group
+        schedule.teacher=teacher
+        schedule.course=course
         if checkpoint is not None:
             checkpoint = Checkpoint.objects.get(pk=checkpoint)
-            shedule.checkpoint.add(checkpoint)
-        shedule.training_group.add(training_groups)
-        shedule.teacher.add(teacher)
-        return shedule
+            schedule.checkpoint=checkpoint
+        schedule.save()
+        return schedule
 
     def update(self, instance, validated_data):
-        shedule = super().update(instance, validated_data)
         request = self.context['request']
         training_group = request.data.get('training_group')
         teacher = request.data.get('teacher')
         checkpoint = request.data.get('checkpoint')
-        if training_group is not None and instance.training_group.id is not training_group:
+        if training_group is not None:
             training_group = TrainingGroup.objects.get(pk=training_group)
-            shedule.training_groups.remove(instance.training_group)
-            shedule.training_groups.add(training_group)
-        if teacher is not None and instance.teacher.id is not teacher:
+            validated_data['training_groups']=training_group
+        if teacher is not None:
             teacher = Teacher.objects.get(pk=teacher)
-            shedule.teacher.remove(instance.teacher)
-            shedule.teacher.add(teacher)
-        if checkpoint is not None and instance.checkpoint is not None and instance.checkpoint.id is not checkpoint:
+            validated_data['teacher']=teacher
+        if checkpoint is not None:
             checkpoint = Checkpoint.objects.get(pk=checkpoint)
-            shedule.checkpoint.remove(instance.checkpoint)
-            shedule.checkpoint.add(checkpoint)
-        if checkpoint is None and instance.checkpoint is not None:
-            shedule.checkpoint.remove(instance.checkpoint)
-        if checkpoint is not None and instance.checkpoint is None:
-            teacher = Checkpoint.objects.get(pk=checkpoint)
-            shedule.checkpoint.add(checkpoint)
-        return shedule 
+            validated_data['checkpoint']=checkpoint
+        if checkpoint is None:
+            validated_data['checkpoint']=None
+        schedule = super().update(instance, validated_data)
+        return schedule 
