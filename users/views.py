3bf50c77e6c995cac2c1470554edafbf3dc56311
x3mart <x3mat@gmail.com>
2021-10-21 15:20:04 +0300

    student attendances action added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
+from rest_framework.response import Response
+
+from schedule.models import Schedule
 from .models import Student, Teacher
 from programs.models import TrainingGroup
 from .serializers import LkStudentSerializer, LkTeacherSerializer
@@ -6,12 +9,15 @@ from rest_framework import viewsets
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import action
 
 
 class LkUserPermission(BasePermission):
     def has_permission(self, request, view):
         if view.action in ['create', 'destroy',]:
             return request.auth and request.user.is_staff
+        if view.action == 'attendances':
+            return request.auth and (request.user.is_staff or request.user.is_teacher)
         return request.auth           
 
     def has_object_permission(self, request, view, obj):
@@ -30,6 +36,20 @@ class LkStudentViewSet(viewsets.ModelViewSet):
         if self.action == 'list' and training_group is not None:
             return Student.objects.filter(training_group=training_group)
         return super().get_queryset()
+    
+    @action(["patch"], detail=True)
+    def attendances(self, request, *args, **kwargs):
+        student = kwargs.get('pk')
+        schedule = request.data.get('schedule')
+        attendance = request.data.get('attendance')
+        student = Student.objects.get(pk=student)
+        schedule = Schedule.objects.get(pk=schedule)
+        if attendance:
+            schedule.visited_students.add(student)
+        else:
+            schedule.visited_students.remove(student)
+        return Response('Ok', status=200)
+
 
 class LkTeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
