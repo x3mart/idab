bd84e9bcc7e24e10de1ed8cf4b916d9e4508ae94
x3mart <x3mat@gmail.com>
2022-03-16 15:01:36 +0300

    tasks and solution fix

--- tasks/admin.py
+++ tasks/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from tasks.models import Solution, Task
 
 # Register your models here.
-admin.site.register(Solution)
-admin.site.register(Task)
+# admin.site.register(Solution)
+# admin.site.register(Task)
