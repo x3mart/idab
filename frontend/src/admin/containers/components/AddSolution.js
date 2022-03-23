25c2dfe9a13c1e7ba816c03571013270d2f4eb43
viperovm <viperovm@gmail.com>
2022-03-23 12:17:31 +0300

    Task minor fixes3 + build

--- frontend/src/admin/containers/components/AddSolution.js
+++ frontend/src/admin/containers/components/AddSolution.js
@@ -65,7 +65,7 @@ const AddSolution = ({task, add_solution}) => {
                       Решение:
                     </h5>
                     <p>
-                      {task.solution.description}
+                      {task && task.solution && task.solution.description}
                     </p>
                   </div>
                   {task && task.solution && task.solution.file && (<div className="solution-file">
@@ -77,7 +77,7 @@ const AddSolution = ({task, add_solution}) => {
                 </div>
                 <div className="mark-wrapper">
                   <h5>Оценка:</h5>
-                  <input type="number" disabled value={task.solution.mark}/>
+                  <input type="number" disabled value={task && task.solution && task.solution.mark}/>
                   <h5>баллов</h5>
                 </div>
               </div>
